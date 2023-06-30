80bb62c801afd7744cff52cce1ad28e4fbe40c67
Inés <201806713@alu.comillas.edu>
2023-06-30 18:25:28 +0200

    dash

---
+++ dashboard/dashMain.py
@@ -0,0 +1,51 @@
+# Importamos las librerias mínimas necesarias
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+
+#from dash_bootstrap_templates import ThemeSwitchAIO
+
+## App
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.MINTY], use_pages=True)
+app.title="TFG"
+
+# Import your pages here
+#import pages.dashMovies
+
+app.layout = dbc.Container(
+    children = [
+        html.Br(),
+        html.H1("Recommender Systems",style={'fontSize': 44}),
+        html.Hr(),
+
+        ## COMPONENTES INTERACTIVAS
+        #ThemeSwitchAIO(aio_id="theme", themes=[dbc.themes.MINTY, dbc.themes.CYBORG]),
+        # Pages Navigator
+        dbc.Nav(
+                children = [
+                    dbc.NavLink([
+                        html.Div(page["name"]),
+                    ],
+                    href=page["path"],
+                    active="exact",
+                    )
+                    for page in dash.page_registry.values()
+                ],
+                pills = True,
+        ),
+
+        html.Div(dash.page_container),
+
+    ], fluid = True)
+
+
+
+
+########################################################################################################################
+########################################################################################################################
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+########################################################################################################################
+########################################################################################################################
